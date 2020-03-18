ad53f0f2097af837ecd8b27f30c40b3efcd6b293
Adamczyk Piotr <adamczyk.piter@outlook.com>
2020-03-18 10:59:23 +0100

    Added linux build script
    Ubuntu 18.04 & Debian compatible

---
+++ linux_build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ "$1" = "install" ]
+then
+	sudo apt-get -y update
+	sudo apt-get -y upgrade
+	sudo apt-get -y install libopencv-dev
+fi
+
+flags="-O3"
+
+g++ src/*.cpp -o app $flags `pkg-config --cflags --libs opencv`
--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,7 @@
 int main() {
 	cv::Mat img = cv::Mat(480, 640, CV_8UC3, cv::Scalar(0, 0, 255));
 	cv::imshow("640x480 Red image", img);
-	cv::waitKey(1);
-	std::cin.get();
+	cv::waitKey(0);
 
 	return 0;
 }
